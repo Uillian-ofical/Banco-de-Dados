a04f41da5ca5744fe47914842e0856225fbc7aba
Uillian-ofical <uillian.gvalerio@gmail.com>
2020-10-25 18:45:06 -0300

    Subindo DQL joins pets

--- Banco de dados/PetsClinica/DQLsql.sql
+++ Banco de dados/PetsClinica/DQLsql.sql
@@ -18,3 +18,19 @@ SELECT * FROM Dono WHERE IdDono = 1;
 SELECT * FROM Pet WHERE IdPet = 2;
 SELECT * FROM Veterinario WHERE IdVeterinario = 1;
 SELECT * FROM Atendimento WHERE IdAtendimento = 2;
+
+SELECT Pet.Nome, 
+Veterinario.Nome, 
+Atendimento.DataAtendimento, 
+Atendimento.Descricao FROM Atendimento
+	
+	INNER JOIN Pet ON Atendimento.IdPet = Pet.IdPet
+	INNER JOIN Veterinario ON Atendimento.IdVeterinario = Veterinario.IdVeterinario;
+
+SELECT Pet.Nome,
+Pet.DataNascimento, 
+Raca.NomeRaca, 
+Dono.NomeDono FROM Pet
+	
+	INNER JOIN Dono ON Pet.IdDono = Dono.IdDono
+	INNER JOIN Raca ON Pet.IdRaca = Raca.IdRaca;
