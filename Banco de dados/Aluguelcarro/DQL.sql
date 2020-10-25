e074b048438d606f29e8dfa1ae8e7ef39580308a
Uillian-ofical <uillian.gvalerio@gmail.com>
2020-10-25 15:51:50 -0300

    subindo DQL  aluguelcarro

---
+++ Banco de dados/Aluguelcarro/DQL.sql
@@ -0,0 +1,17 @@
+/* DQL - Data Query Language */
+
+USE Aluguelcarro;
+
+SELECT * FROM Marca;
+SELECT * FROM Modelo;
+SELECT * FROM Cliente;
+SELECT * FROM Veiculo;
+SELECT * FROM DDD;
+SELECT * FROM Telefone;
+
+SELECT * FROM Marca WHERE IdMarca = 1;
+SELECT * FROM Modelo WHERE IdModelo = 2;
+SELECT * FROM DDD WHERE IdDDD = 2;
+SELECT * FROM Telefone WHERE IdTelefone = 1;
+SELECT * FROM Cliente WHERE IdCliente = 1;
+SELECT * FROM Veiculo WHERE IdVeiculo = 1;
