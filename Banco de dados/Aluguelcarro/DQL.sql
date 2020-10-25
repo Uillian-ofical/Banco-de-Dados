33f1cdf87e4bff3ad5416dce66f8ee29282c77aa
Uillian-ofical <uillian.gvalerio@gmail.com>
2020-10-25 15:59:16 -0300

    subindo joins DQL aluguelcarro

--- Banco de dados/Aluguelcarro/DQL.sql
+++ Banco de dados/Aluguelcarro/DQL.sql
@@ -15,3 +15,13 @@ SELECT * FROM DDD WHERE IdDDD = 2;
 SELECT * FROM Telefone WHERE IdTelefone = 1;
 SELECT * FROM Cliente WHERE IdCliente = 1;
 SELECT * FROM Veiculo WHERE IdVeiculo = 1;
+
+
+-- Selecionando dados com INNER JOIN
+SELECT Marca.Nome as 'Marca', 
+Modelo.Nome as 'Modelo', Veiculo.Cor, Veiculo.Placa, Veiculo.Ignicao, 
+Cliente.Nome as 'Cliente' FROM Veiculo
+
+	INNER JOIN Cliente ON Veiculo.IdCliente = Cliente.IdCliente
+	INNER JOIN Modelo ON Veiculo.IdModelo = Modelo.IdModelo
+	INNER JOIN Marca ON Modelo.IdMarca = Marca.IdMarca
