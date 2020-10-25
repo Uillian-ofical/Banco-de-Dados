2b926148a9256d350768a1fad9f0c7f44cfe1e59
Uillian-ofical <uillian.gvalerio@gmail.com>
2020-10-25 18:00:42 -0300

    subindo DQL joins optus

--- Banco de dados/optus/DQL.sql
+++ Banco de dados/optus/DQL.sql
@@ -16,3 +16,12 @@ SELECT * FROM Album WHERE IdAlbum = 1;
 SELECT * FROM EstilosAlbum WHERE IdEstilosAlbum = 1;
 SELECT * FROM TipoUsuario WHERE IdTipoUsuario = 1;
 SELECT * FROM Usuario WHERE IdUsuario = 2;
+
+-- Selecionando dados com inner joins
+SELECT Album.Nome as 'Album', 
+Artista.Nome as 'Artista', 
+Estilo.NomeEstilo as 'Estilo do Album' FROM EstilosAlbum
+   
+	INNER JOIN Album ON EstilosAlbum.IdAlbum = Album.IdAlbum
+	INNER JOIN Estilo ON EstilosAlbum.IdEstilo = Estilo.IdEstilo
+    INNER JOIN Artista ON Album.IdArtista = Artista.IdArtista
